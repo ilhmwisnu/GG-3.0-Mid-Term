7d2e0ed6b3284d7ff14efadfc4ac7005350b6f08
ilhmwisnu <ilhamwisnu42@gmail.com>
2023-07-25 01:37:12 +0700

    add video controller

---
+++ controller/video_controller.js
@@ -0,0 +1,38 @@
+
+const Video = require('../models/video');
+
+const getAll = async (req,res)=>{
+  try {
+    let data = await Video.find()
+
+    res.json({
+      message : "Success get data",
+      data : data ?? []
+    })
+  } catch (error) {
+    res.status(400).json({
+      message : error.message
+    })
+  }
+}
+
+const getById = async (req,res) => {
+  try {
+    let id = req.params.id
+    let data = await Video.findById(id)
+
+    if (!data) return res.status(404).json({ message : "Data not found" })
+
+    res.json({
+      message : "Success get data",
+      data : data
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      message : error.message
+    })
+  }
+}
+
+module.exports = { getAll, getById }
