ae05a02048a5b92002162ab1e10f685b65d3b7ee
ilhmwisnu <ilhamwisnu42@gmail.com>
2023-07-25 02:54:03 +0700

    adjustment video model

--- models/video.js
+++ models/video.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const VideoSchema = mongoose.Schema({
   img_url : String,
-  title : String
+  title : String,
+  video_url : String
 }) 
 
 module.exports = mongoose.model("Video", VideoSchema)
